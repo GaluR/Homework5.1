de8353d442bbb44d5156dc11472bc7f4c3b2da90
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-18 12:38:33 +0200

    Correct Test class.

--- src/TestRoom.java
+++ src/TestRoom.java
@@ -1,21 +1,32 @@
-public class TestRoom {
-    public static void main(String[] args) {
+public class TestRoom
+{
+    public void showInfo(Room room){
+        System.out.println("Pokój o powierzchni: " + room.surface + " aktualnej temperaturze: " + room.temperature + " Czy posiada klimatyzator?: " + room.isConditioner);
+    }
+
+    public static void main(String[] args)
+    {
         Room room = new Room(53.5, 24, true);
         TestRoom test1 = new TestRoom();
 
-        public void showinfo() {
-            System.out.println("Pokój o powierzchni: " + room.surface + " aktualnej temperaturze: " + room.temperature + " Czy posiada klimatyzator?: " + room.isConditioner);
-        }
 
+
+        test1.showInfo(room);
+        System.out.println(room.lowerTemperature());
+        test1.showInfo(room);
+        System.out.println(room.lowerTemperature());
+        test1.showInfo(room);
+        System.out.println(room.lowerTemperature());
+        test1.showInfo(room);
+        System.out.println(room.lowerTemperature());
+        System.out.println(room.lowerTemperature());
+        test1.showInfo(room);
         System.out.println(room.lowerTemperature());
         System.out.println(room.lowerTemperature());
         System.out.println(room.lowerTemperature());
+        test1.showInfo(room);
         System.out.println(room.lowerTemperature());
         System.out.println(room.lowerTemperature());
-        System.out.println(room.lowerTemperature());
-        System.out.println(room.lowerTemperature());
-        System.out.println(room.lowerTemperature());
-        System.out.println(room.lowerTemperature());
-        System.out.println(room.lowerTemperature());
+        test1.showInfo(room);
     }
 }
